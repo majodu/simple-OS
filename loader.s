19650c8505674c3773c0715037d762bb7e68aea8
Mattias Duffy <mduffy8@gmu.edu>
2017-06-13 20:50:48 -0400

    First Commit. finished 3.2.0 WORKING

---
+++ loader.s
@@ -0,0 +1,34 @@
+global loader                   ; the entry symbol for ELF
+
+MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
+FLAGS        equ 0x0            ; multiboot flags
+CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
+KERNEL_STACK_SIZE equ 4096
+
+                                    ; (magic number + checksum + flags should equal 0)
+
+section .text:                  ; start of the text (code) section
+align 4                         ; the code must be 4 byte aligned
+	dd MAGIC_NUMBER             ; write the magic number to the machine code,
+	dd FLAGS                    ; the flags,
+	dd CHECKSUM                 ; and the checksum
+	dd KERNEL_STACK_SIZE
+	
+section .bss:
+align 4
+kernel_stack:
+	resb KERNEL_STACK_SIZE
+
+
+loader:                         ; the loader label (defined as entry point in linker script)
+	mov esp, kernel_stack + KERNEL_STACK_SIZE
+	mov eax, 0xCAFEBABE
+	extern sum_of_three
+	push dword 3
+	push dword 2
+	push dword 1
+	call sum_of_three
+
+.loop:
+	jmp .loop
+
